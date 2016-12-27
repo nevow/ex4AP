5812f6070ec492c55176c5eb9dc7980c6ac71c46
nevow <nevo12345@gmail.com>
2016-12-27 19:37:31 +0200

    server and client classes

---
+++ src/server/Server.cpp
@@ -0,0 +1,33 @@
+//
+// Created by nevo on 27/12/16.
+//
+
+#include <iostream>
+#include "../sockets/Udp.h"
+#include <unistd.h>
+
+int main() {
+    std::cout << "Hello, from server" << std::endl;
+
+    Udp udp(1, 5555);
+    udp.initialize();
+
+    char buffer[1024];
+    udp.reciveData(buffer, sizeof(buffer));
+    cout << buffer << endl;
+    udp.sendData("sup?");
+
+
+//
+//    usleep(5000);
+    Udp udp2(1, 5554);
+    udp2.initialize();
+
+    char buffer2[1024];
+    udp2.reciveData(buffer2, sizeof(buffer2));
+    cout << buffer2 << endl;
+    udp2.sendData("sup?");
+
+    // support more than one client?
+    return 0;
+}
