7f4f28239809c06701f289587cbd7920293420e9
nevow <nevo12345@gmail.com>
2016-12-31 15:38:58 +0200

    checking memory

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -67,43 +67,8 @@ int main(int argc, char *argv[]) {
         }
         // if the client received the advance order
     } while (strcmp(buffer, "exit"));
-    // do while the server still sends orders different from the exit order "exit"
-    /*do {
-        if (ti == NULL) {
-            sock->sendData("waiting_for_trip");      // tell the server that the client is waiting
-            cout << "sent waiting for trips" << endl;
-
-            // no trip info to move with
-            /*if (!strcmp(buffer, "9")) {
-                continue;
-            }*/
-    // deserialize the trip info from the server
-    /*ti = DataSender<TripInfo>::receiveData(sock);
-    cout << "received trip" << endl;
-    driver->setTi(ti);                     // set the driver with the trip info
-}
-//sock->sendData("waiting_for_orders");      // tell the server that the client is waiting
-cout << "sent waiting for orders" << endl;
-sock->receiveData(buffer, sizeof(buffer));  // wait to receive the orders from the server
-
-// if the client received the advance order
-if (!strcmp(buffer, "9")) {
-    clock++;
-    driver->moveOneStep(clock);                 // move the driver one step
-    cout << *(driver->getCab()->getLocation()->getP());
-
-    if (driver->getTi()->checkEnd(cab->getLocation()->getP())) { // if reached the end
-        delete ti;
-        driver->setTi(NULL);
-        ti = NULL;
-    }
-}
-} while (strcmp(buffer, "exit"));*/
 
     // delete all objects in the client
-    if (ti != NULL) {
-        delete ti;
-    }
     delete cab;
     delete driver;
     return 0;
